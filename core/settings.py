9cfc2de50cdb9a773c91672a9211c0427fb228f3
ArceusLegend <markoskap13@gmail.com>
2023-10-11 12:20:40 +0300

    begin working on deploying with apache

--- core/settings.py
+++ core/settings.py
@@ -47,15 +47,17 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mod_wsgi.server',
     'store',
     'cart',
     'users',
     'payment',
-    'orders'
+    'orders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
