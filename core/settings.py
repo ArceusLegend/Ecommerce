6474987e8c148487172b16a4cdc6a8eded8a6b26
ArceusLegend <markoskap13@gmail.com>
2023-10-13 12:13:33 +0300

    Removed wsgi as it's not being used

--- core/settings.py
+++ core/settings.py
@@ -47,7 +47,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'mod_wsgi.server',
     'store',
     'cart',
     'users',
