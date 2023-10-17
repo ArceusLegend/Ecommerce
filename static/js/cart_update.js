b7f6c407cd1316b2232b4d015193b7de55bb81e3
ArceusLegend <markoskap13@gmail.com>
2023-10-17 10:03:07 +0300

    linted the html files and updated requirements.txt

--- static/js/cart_update.js
+++ static/js/cart_update.js
@@ -41,3 +41,22 @@ $(document).on("click", ".update-button", function (e) {
     error: function (xhr, errmsg, err) {},
   });
 });
+
+// Add item
+$(document).on('click', '#add-button', function(e){
+  e.preventDefault();
+  $.ajax({
+      type: 'POST',
+      url: '{% url "cart:cart_add" %}',
+      data: {
+          productid: $('#add-button').val(),
+          productqty: $('#select').val(),
+          csrfmiddlewaretoken: "{{csrf_token}}",
+          action: 'post'
+      },
+      success: function (json) {
+          document.getElementById("cart-qty").innerHTML = json.qty
+      },
+      error: function (xhr, errmsg, err) {}
+  });
+});
